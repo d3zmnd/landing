bbb05408596b6546263ee41c09d05113d9270e97
Vitaly Davidenko <35844879+d3zmnd@users.noreply.github.com>
2018-02-18 17:49:24 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// work 1
+// var day = prompt('Какой сегодня день?');
+// alert (day);
+
+// work 2
+// *1
+// var role = prompt('Введите имя пользователя','Гость, Директор или оставить пустым');
+// if (role == 'Гость') { 
+// alert('Привет');
+// } else if (role == 'Директор') {
+//   alert('Здравствуйте');
+// } else if (role == '') {
+//   alert('Познакомимся?');
+// } else {
+//   alert('');
+// }
+
+// *2
+// var role = prompt('Имя пользователя')
+// role == 'Гость'? alert('Привет'):role == 'Директор'? alert ('Здравствуйте'): role==''? alert('Познакомимся?'):alert('');
+
+// work 3
+// var name = prompt('Какое официальное названия JavaScript?');
+// if (name =='EcmaScript'){
+// 	alert('Совершенно верно!');
+// } else alert('Не знаете EcmaScript?');
+
+// work 4
+
+// var role = prompt('Кто пришел?');
+// if (role == 'Админ') { 
+// 	var pass = prompt('Пароль')
+// 	if( pass = 'Черный Властелин'){
+// 		alert('Добро пожаловать!');
+// 	}
+// } else if (role == 'Отмена') {
+//   alert('Вход отменен');
+//   } else {
+//   alert('Я вас не знаю');
+// }
+
+// work 5
+
